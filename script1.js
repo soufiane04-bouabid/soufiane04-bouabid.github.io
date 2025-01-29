d48ea023a9989cadb0bbd4407662cdc7b6cf4819
soufiane04-bouabid <soufiane.bouabid@etudiant.univ-reims.fr>
2025-01-29 14:14:14 +0100

    Update script1.js

--- script1.js
+++ script1.js
@@ -81,6 +81,18 @@ updateCarousel(currentIndex);
 // Initialize with the first project
 updateCarousel(currentIndex);
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker enregistré avec succès :', registration);
+        })
+        .catch((error) => {
+          console.log('Erreur d\'enregistrement du Service Worker :', error);
+        });
+    });
+  }
+
 
 
 
